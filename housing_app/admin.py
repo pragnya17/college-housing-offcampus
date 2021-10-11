c0323e7734e3d3da59f65608bb6cdce016e967cb
andrewgillikin <89940406+andrewgillikin@users.noreply.github.com>
2021-10-11 17:04:35 -0400

    auth test cases

--- housing_app/admin.py
+++ housing_app/admin.py
@@ -9,7 +9,6 @@ class UserAdmin(BaseUserAdmin):
         (None, {'fields': ('email', 'password', 'name', 'last_login')}),
         ('Permissions', {'fields': (
             'is_active', 
-            'is_staff', 
             'is_superuser',
             'groups', 
             'user_permissions',
@@ -25,8 +24,8 @@ class UserAdmin(BaseUserAdmin):
         ),
     )
 
-    list_display = ('email', 'name', 'is_staff', 'last_login')
-    list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups')
+    list_display = ('email', 'name', 'last_login')
+    list_filter = ('is_superuser', 'is_active', 'groups')
     search_fields = ('email',)
     ordering = ('email',)
     filter_horizontal = ('groups', 'user_permissions',)
