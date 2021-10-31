9806de6534a22e6ed944b675f0d77475fe025bcc
pragnya17 <pp8sdv@virginia.edu>
2021-10-31 16:55:10 -0400

    Change parking property

--- housing_app/models.py
+++ housing_app/models.py
@@ -59,7 +59,7 @@ class Property(models.Model):
     total_price = models.DecimalField(max_digits=7, decimal_places=2)
     distance = models.DecimalField(max_digits=4, decimal_places=2, default=0)
     furnished = models.CharField(max_length=3, default="")
-    parking = models.CharField(max_length=3, default="")
+    parking = models.CharField(max_length=20, default="")
     rooms = models.TextField(default="")
     address = models.CharField(max_length=200)
     services = models.TextField(default="")
