034da061ab1d6ca24e8f019c3a8142929830b524
andrewgillikin <89940406+andrewgillikin@users.noreply.github.com>
2021-11-06 16:06:36 -0400

    django-filter installation

--- housing/settings.py
+++ housing/settings.py
@@ -162,7 +162,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
