e2cdf85f37ffcd7f5f5a8458bf077df689a3bf9c
kiyanzewer <46326938+kiyanzewer@users.noreply.github.com>
2021-11-14 01:08:37 -0500

    Revert "added ability to upload pictures and made minor change on properties …"

--- housing_app/models.py
+++ housing_app/models.py
@@ -55,7 +55,7 @@ class User(AbstractBaseUser, PermissionsMixin):
 
 class Property(models.Model):
     title = models.CharField(max_length=200)
-    description = models.CharField(max_length=75)
+    description = models.TextField()
     total_price = models.DecimalField(max_digits=7, decimal_places=2)
     distance = models.DecimalField(max_digits=4, decimal_places=2, default=0)
     furnished = models.CharField(max_length=3, default="No")
@@ -66,7 +66,6 @@ class Property(models.Model):
     services = models.TextField(default="")
     amenities = models.TextField(default="")
     floorplan = models.ImageField(upload_to='floorplans')
-    picture = models.ImageField(upload_to='pictures', default="")
 
     def __str__(self):
       if self.title == '':
