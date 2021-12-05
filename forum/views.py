798c7b53c1efa3577d6e8e9301e96c3fc891e66a
chloeestrada <60328305+chloeestrada@users.noreply.github.com>
2021-12-04 20:14:19 -0500

    Additional references

--- forum/views.py
+++ forum/views.py
@@ -5,35 +5,45 @@ from .models import *
 from .forms import *
 from django.contrib import messages
 
-# Create your views here.
+# REFERENCES
+# Title: How to make add replies to comments in django
+# Date accessed: 11/26/2021
+# Date created: 7/2017
+# Link: https://stackoverflow.com/questions/44837733/how-to-make-add-replies-to-comments-in-django
 
-#def forumHome(request):
-#    return render(request, 'forum/forum.html')
+# Title: Create a Discussion Forum in Python Django
+# Date accessed: 11/21/21
+# Author: Project Gurukul
+# Link: https://projectgurukul.org/python-django-online-discussion-forum/
+
+# Title: Django Messages Framework
+# Date accessed: 12/1/2021
+# Date created: 6/2/2020
+# Author: Jaysha from Ordinary Coders
+# Link: https://ordinarycoders.com/blog/article/django-messages-framework
+
+# Title: Create a Discussion Forum
+# Date accessed: 11/9/2021
+# Author: Data Flair
+# Link: https://data-flair.training/blogs/discussion-forum-python-django/
+
+# Title: Django - Taking values from POST request
+# Date accessed: 11/15/21
+# Date created: 7/2012
+# Link: https://stackoverflow.com/questions/11336548/django-taking-values-from-post-request/11336580
 
 def forumHome(request):
     forums=myForum.objects.all()
     count=forums.count()
-    #discussions=[]
     myDiscussions = Discussion.objects.all()
 
-
-    #discuss = request.POST.get('Reply', False)
-    #name = request.POST.get('Name', False)
-    #specificForumID = request.POST.get('forum')
-    #x = myForum.objects.get(name=request.POST.get('forum', False))
-    #form = Discussion(myForum = x, discuss = discuss, name = name)
     form = CreateInDiscussion()
     if request.method == 'POST':
         request.Get_Mutable = True
         print(request.POST.get('forum', ''))
         print(request.POST.get('discuss', ''))
         print(request.POST.get('name', ''))
-        #hereForum = request.POST[2]
-        #print(request.POST['myForum.forum'])
-        #theName = request.POST.get('name', False)  # ask the user for this in case they want to change their name to Ananymous # good
-        #theDiscuss = request.POST.get('discuss', False) # ask the user for this  # good
-        #theRightForum = request.POST.get('myForum', False)   # don't ask the user for this
-        #form = CreateInDiscussion(discuss = theDiscuss, name = theName, myForum = theRightForum)
+        
         form = CreateInDiscussion()
         form = CreateInDiscussion(request.POST)
         form.data = form.data.copy()
@@ -42,7 +52,6 @@ def forumHome(request):
         form.data['name'] = request.POST.get('name', '')
         print("After seting the form fields")
         #print(form)
-
 
         if form.is_valid():
             form.save()
@@ -55,8 +64,6 @@ def forumHome(request):
               'myDiscussions':myDiscussions,
               'form':form}
     return render(request, 'forum/forum.html', context)
-    #return HttpResponseRedirect(request,'forum:forum.html',context)
-    #return HttpResponseRedirect(reverse('forum:addInForum'))
 
 def addInForum(request):
     form = CreateInForum()
@@ -73,16 +80,3 @@ def addInForum(request):
             return redirect('/forum')
     context ={'form':form}
     return render(request,'forum/addInForum.html',context)
-    #return render(request, 'forum/templates/forum/addInForum.html' ,context)
-    #return render(request, 'housing/properties.html', context)
-
-#def addInDiscussion(request):
-    ##form = CreateInDiscussion()
-    #discuss = request.POST.get('Reply', False)
-    #name = request.POST.get('Name', False)
-    #x = myForum.objects.get(name=request.POST.get('forum', False))
-
-    #form = Discussion(myForum = x, discuss = discuss, name = name)
-    #form.save()
-    #context = {'form':form}
-    #return render(request,'forum/addInDiscussion.html', context)
