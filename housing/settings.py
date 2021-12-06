f4af132072a0fdb4dfc2da61ec03fa3f43f1a945
pragnya17 <pp8sdv@virginia.edu>
2021-12-06 13:23:06 -0500

    debug true for now

--- housing/settings.py
+++ housing/settings.py
@@ -30,7 +30,7 @@ SESSION_COOKIE_SECURE = True
 SECURE_SSL_REDIRECT = True
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
